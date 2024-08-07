61042d22318ba0900930ec44ac9b51dd64e23e31
Avdhesh singh <rajputavdhesh2000@gmail.com>
2024-08-07 11:09:03 +0530

    added code

--- src/elseif.java
+++ src/elseif.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class elseif {
-}
+public class elseif {
+
+        public static void main(String[] args) {
+            int number = 16;
+
+
+            // Check if the number is positive, negative, or zero
+            if (number > 0) {
+                System.out.println("The number is positive.");
+            } else if (number < 0) {
+                System.out.println("The number is negative.");
+            } else {
+                System.out.println("The number is zero.");
+            }
+
+        }
+    }
+
